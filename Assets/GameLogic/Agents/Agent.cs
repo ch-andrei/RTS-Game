03eaba09bf3c5a9e160944559c16cd64bd38734c
Andrei Chubarau <andrei.chubarau@irystec.com>
2018-02-11 19:08:03 -0500

    Added a pathfinding test scene using unity navmesh

---
+++ Assets/GameLogic/Agents/Agent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class Agent : MonoBehaviour {
+
+    public GameObject goal;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.destination = goal.transform.position;
+    }
+}
